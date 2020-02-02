0940eaf934d1e2c5b4c8ec3d88cddd5a477d5650
Steven 'Viel' Nataniel <ravielz20@gmail.com>
2020-02-02 08:30:44 +0700

    Rehagana KCS - 16519380 - Apply event to robot

--- robot.c
+++ robot.c
@@ -1,51 +1,53 @@
 #include "robot.h"
 #include "utils.h"
+#include "event.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 Robot initRobot(){
     Robot r;
     r.money = 50;
     r.social = 0;
 
-    int constant1 = random(25,42);
-    int constant2 = random(20,40);
+    int constant1 = random(10,15);
+    int constant2 = random(12,17);
     int bonus = random(1,12);
     int bonus2 = random(1,13);
 
     r.happiness = constant1+25-bonus;
-    r.hygiene = 50-constant1+bonus;
+    r.hygiene = 25-constant1+bonus;
     r.hunger = constant2+25-bonus2;
-    r.health = 50-constant2+bonus2;
+    r.health = 25-constant2+bonus2;
 
     r.deathCause = "";
     r.isDead = false;
     return r;
 }
 
-bool doAct(Robot *r, Activity *a){
-    if(a->money < 0 && r->money < abs(a->money)){
+bool doAct(Robot *r, Activity a){
+    if(a.money < 0 && r->money < abs(a.money)){
         return false;
     }
-    r->money += a->money;
-    r->happiness += a->happiness;
-    r->hygiene += a->hygiene;
-    r->social += a->social;
-    r->hunger += a->hunger;
-    r->health += a->health;
+    r->money += a.money;
+    r->happiness += a.happiness;
+    r->hygiene += a.hygiene;
+    r->social += a.social;
+    r->hunger += a.hunger;
+    r->health += a.health;
 
-    if (r->happiness < 0){
+    if (r->happiness <= 0){
         r->isDead = true;
         r->deathCause = "Unhappy";
     }
-    if (r->hygiene < 0){
+    if (r->hygiene <= 0){
         r->isDead = true;
         r->deathCause = "Unhygienic";
     }
-    if (r->hunger < 0){
+    if (r->hunger <= 0){
         r->isDead = true;
         r->deathCause = "Starvation";
     }
-    if(r->health < 0){
+    if(r->health <= 0){
         r->isDead = true;
         r->deathCause = "Death";
     }
@@ -56,6 +58,38 @@ bool doAct(Robot *r, Activity *a){
     r->health = (r->health > 100) ? 100 : r->health;
 
     return true;
+}
+
+void doReversedAct(Robot *r, Activity a){
+    r->money -= abs(a.money);
+    r->happiness -= abs(a.happiness);
+    r->hygiene -= abs(a.hygiene);
+    r->social -= abs(a.social);
+    r->hunger -= abs(a.hunger);
+    r->health -= abs(a.health);
+
+    if (r->happiness <= 0){
+        r->isDead = true;
+        r->deathCause = "Unhappy";
+    }
+    if (r->hygiene <= 0){
+        r->isDead = true;
+        r->deathCause = "Unhygienic";
+    }
+    if (r->hunger <= 0){
+        r->isDead = true;
+        r->deathCause = "Starvation";
+    }
+    if(r->health <= 0){
+        r->isDead = true;
+        r->deathCause = "Death";
+    }
+
+    r->happiness = (r->happiness > 100) ? 100 : r->happiness;
+    r->hygiene = (r->hygiene > 100) ? 100 : r->hygiene;
+    r->hunger = (r->hunger > 100) ? 100 : r->hunger;
+    r->health = (r->health > 100) ? 100 : r->health;
+
 }
 
 void showStatus(Robot r){
@@ -87,4 +121,31 @@ void showStatus(Robot r){
         printf("-");
     }
     return;
+}
+
+void respondEvent(Robot *r, Event *e){
+    Activity act = e->activity;
+    if((random(1,2*random(2,6)) % 2) == 1){
+        printf("\n%s\n[ ", e->negative);
+        if(act.money > 0) printf("-%d$ ", act.money);
+        if(act.happiness > 0) printf("-%d Happiness ", act.happiness);
+        if(act.hygiene > 0) printf("-%d Hygiene ", act.hygiene);
+        if(act.hunger > 0) printf("-%d Hunger ", act.hunger);
+        if(act.social > 0) printf("-%d Social ", act.social);
+        if(act.health > 0) printf("-%d Health ", act.health);
+        printf("]\n");
+
+        doReversedAct(r, act);
+    } else {
+        printf("\n%s\n[ ", e->positive);
+        if(act.money > 0) printf("+%d$ ", act.money);
+        if(act.happiness > 0) printf("+%d Happiness ", act.happiness);
+        if(act.hygiene > 0) printf("+%d Hygiene ", act.hygiene);
+        if(act.hunger > 0) printf("+%d Hunger ", act.hunger);
+        if(act.social > 0) printf("+%d Social ", act.social);
+        if(act.health > 0) printf("+%d Health ", act.health);
+        printf("]\n");
+
+        doAct(r, act);
+    }
 }
